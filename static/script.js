efc75d2e073ae2065913c4d8979a2c198cf5d5f5
Dima Kuchin <dima@kuchin.net>
2023-09-12 09:41:53 +0300

    JS refactor

--- static/script.js
+++ static/script.js
@@ -1,134 +1,136 @@
 let selectedConvElem = null;  // Global variable to track selected conversation
 
 function appendConversationsToSidebar(data) {
-    const sidebar = document.getElementById('sidebar');
+    const sidebar = document.getElementById("sidebar");
     let currentGroup = null;
 
-    data.forEach(conv => {
+    data.forEach((conv) => {
         // Check if the conversation belongs to a new group
         if (conv.group !== currentGroup) {
-            // Update the current group
             currentGroup = conv.group;
 
             // Add a group title to the sidebar
-            const groupTitleDiv = document.createElement('div');
-            groupTitleDiv.className = 'p-2 text-gray-700 font-bold';
-            groupTitleDiv.innerText = currentGroup || 'No Group';
-            sidebar.appendChild(groupTitleDiv);
+            sidebar.insertAdjacentHTML("beforeend", `
+                <div class="p-2 text-gray-700 font-bold">
+                    ${currentGroup || "No Group"}
+                </div>
+            `);
         }
 
-        // Create a div for the conversation
-        const convDiv = document.createElement('div');
-        convDiv.className = 'p-2 hover:bg-gray-300 cursor-pointer';
-        const convTitle = conv.title || 'Untitled';
-        convDiv.innerHTML = `<div class="flex justify-between">
-            <span class="mr-2">${convTitle}</span>
-            <small class="text-gray-500 whitespace-nowrap">${conv.created}</small>
-        </div>`;
+        const convTitle = conv.title || "Untitled";
+        sidebar.insertAdjacentHTML("beforeend", `
+            <div class="p-2 hover:bg-gray-300 cursor-pointer flex justify-between" id="conv-${conv.id}">
+                <span class="mr-2">${convTitle}</span>
+                <small class="text-gray-500 whitespace-nowrap">${conv.created}</small>
+            </div>
+        `);
+
         // Add click event listener
-        convDiv.addEventListener('click', function() {
+        const convDiv = document.getElementById(`conv-${conv.id}`);
+        convDiv.addEventListener("click", function () {
             loadMessages(conv.id);
 
-            // Remove the highlight class from the previously selected conversation
             if (selectedConvElem) {
-                selectedConvElem.classList.remove('bg-gray-400');
+                selectedConvElem.classList.remove("bg-gray-400");
             }
-
-            // Add the highlight class to the currently selected conversation
-            this.classList.add('bg-gray-400');
-
-            // Update the selected conversation element
+            this.classList.add("bg-gray-400");
             selectedConvElem = this;
         });
-
-        // Append the conversation div to the sidebar
-        sidebar.appendChild(convDiv);
     });
 }
 
-function loadMessages(convId) {
-    fetch(`/api/conversations/${convId}/messages`)
-        .then(response => response.json())
-        .then(data => {
-            const mainContent = document.getElementById('main-content');
-            mainContent.innerHTML = '';  // Clear previous messages
-            const headerDiv = document.createElement('div');
-            headerDiv.className = 'p-2 border-b text-right';
-            headerDiv.innerHTML = `
-                <a href="https://chat.openai.com/c/${data.conversation_id}" 
-                target="_blank" class="hover:underline">Open in ChatGPT ↗️</a>
-            `;
-            mainContent.appendChild(headerDiv);
+async function loadMessages(convId) {
+    try {
+        const response = await fetch(`/api/conversations/${encodeURIComponent(convId)}/messages`);
+        const data = await response.json();
 
-            const messages = data.messages;
-            messages.forEach(msg => {
-                const msgDiv = document.createElement('div');
-                msgDiv.className = 'p-2 border-b';
-                msgDiv.innerHTML = `
+        const mainContent = document.getElementById("main-content");
+        mainContent.innerHTML = ""; // Clear previous messages
+
+        // Create a header with a link to the conversation
+        mainContent.insertAdjacentHTML('beforeend', `
+            <div class="p-2 border-b text-right">
+                <a href="https://chat.openai.com/c/${data.conversation_id}"
+                target="_blank" rel="noopener noreferrer" class="hover:underline">Open in ChatGPT ↗️</a>
+            </div>
+        `);
+
+        // Populate the main content with messages
+        const messages = data.messages;
+        messages.forEach((msg) => {
+            mainContent.insertAdjacentHTML('beforeend', `
+                <div class="p-2 border-b">
                     <small class="text-gray-500">${msg.created}</small>
                     <br/>
                     <strong>${msg.role}:</strong>
                     <span>${msg.text}</span>
-                `;
-                mainContent.appendChild(msgDiv);
-            });
-
-            // Scroll to the top of the main content area
-            const mainContentWrapper = document.getElementById('main-content-wrapper');
-            mainContentWrapper.scrollTop = 0;
+                </div>
+            `);
         });
+
+        // Scroll to the top of the main content area
+        const mainContentWrapper = document.getElementById("main-content-wrapper");
+        mainContentWrapper.scrollTop = 0;
+    } catch (error) {
+        console.error("Failed to load messages:", error);
+    }
 }
 
-function handleSearchInput(event) {
+async function handleSearchInput(event) {
     if (event.key !== "Enter")
         return;
 
-    const query = searchInput.value;
+    const query = encodeURIComponent(searchInput.value);
 
-    // Perform search based on the query
-    fetch(`/api/search?query=${query}`)
-        .then(response => response.json())
-        .then(data => {
-            const mainContent = document.getElementById('main-content');
-            mainContent.innerHTML = '';  // Clear previous messages
-            data.forEach(msg => {
-                const msgDiv = document.createElement('div');
-                msgDiv.className = 'p-2 border-b';
-                msgDiv.innerHTML = `
+    try {
+        const response = await fetch(`/api/search?query=${query}`);
+        const data = await response.json();
+
+        const mainContent = document.getElementById("main-content");
+        mainContent.innerHTML = ""; // Clear previous messages
+
+        data.forEach((msg) => {
+            mainContent.insertAdjacentHTML('beforeend', `
+                <div class="p-2 border-b">
                     <strong>${msg.role}:</strong>
                     <span>${msg.text}</span>
                     <small class="text-gray-500">${msg.created}</small>
-                `;
-                mainContent.appendChild(msgDiv);
-            });
+                </div>
+            `);
         });
+    } catch (error) {
+        console.error("Search failed:", error);
+    }
 }
 
-function loadConversations() {
-    fetch('/api/conversations')
-        .then(response => response.json())
-        .then(data => {
-            const sidebar = document.getElementById('sidebar');
-            sidebar.innerHTML = '';  // Clear previous conversations
-            appendConversationsToSidebar(data);
-        });
+// Listen for Enter key press on searchInput element
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("keydown", handleSearchInput);
+
+async function loadConversations() {
+    try {
+        const response = await fetch("/api/conversations");
+        const data = await response.json();
+
+        const sidebar = document.getElementById("sidebar");
+        sidebar.innerHTML = ""; // Clear previous conversations
+        appendConversationsToSidebar(data);
+    } catch (error) {
+        console.error("Failed to load conversations:", error);
+    }
 }
 
-function loadActivityGraph() {
-    fetch('/api/activity')
-      .then(response => response.json())
-      .then(data => {
-        buildActivityGraph(document.getElementById('activity-graph'), {
-            data: data,
-        });
-    });          
+async function loadActivityGraph() {
+    try {
+        const response = await fetch("/api/activity");
+        const data = await response.json();
+        buildActivityGraph(document.getElementById("activity-graph"), { data: data });
+    } catch (error) {
+        console.error("Failed to load activity graph:", error);
+    }
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
     loadConversations();
     loadActivityGraph();
 });
-
-const searchInput = document.getElementById('search-input');
-searchInput.addEventListener('keydown', handleSearchInput);
-
